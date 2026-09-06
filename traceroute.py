821548506ed47ea242297c38661c4f25aafa3348
Ben Tonnos <bentonnos1@berkeley.edu>
2026-09-06 12:53:58 -0700

    fixed thing

--- traceroute.py
+++ traceroute.py
@@ -72,9 +72,9 @@ class ICMP:
 
     def __init__(self, buffer: bytes):
         b = ''.join(format(byte, '08b') for byte in [*buffer])
-        self.type = int(b[160, 168], 2)
-        self.code = int(b[168, 176], 2)
-        self.cksum = int(b[176, 192], 2)
+        self.type = int(b[160:168], 2)
+        self.code = int(b[168:176], 2)
+        self.cksum = int(b[176:192], 2)
 
     def __str__(self) -> str:
         return f"ICMP (type {self.type}, code {self.code}, " + \
